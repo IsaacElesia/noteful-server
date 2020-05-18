7e6bfdc9cc5a20e00beef5958cc4a8a70ca805ce
Isaac Elesia <isaacelesia@gmail.com>
2020-05-18 15:12:42 -0500

    update

--- src/app.js
+++ src/app.js
@@ -11,12 +11,9 @@ const noteRouter = require('./noteRouter');
 const folderRouter = require('./folderRouter');
 const { NODE_ENV, API_TOKEN } = require('./config');
 
-
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 // middleware
 
@@ -24,39 +21,39 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function requireAuth(req, res, next) {
-  const authValue = req.get('Authorization') || ' ';
+/* app.use(function requireAuth(req, res, next) {
+	const authValue = req.get('Authorization') || ' ';
 
-  //verify bearer
-  if (!authValue.toLowerCase().startsWith('bearer')) {
-    return res.status(400).json({ error: 'Missing bearer token' });
-  }
+	//verify bearer
+	if (!authValue.toLowerCase().startsWith('bearer')) {
+		return res.status(400).json({ error: 'Missing bearer token' });
+	}
 
-  const token = authValue.split(' ')[1];
+	const token = authValue.split(' ')[1];
 
-  if (token !== API_TOKEN) {
-    return res.status(401).json({ error: 'Invalid token' });
-  }
+	if (token !== API_TOKEN) {
+		return res.status(401).json({ error: 'Invalid token' });
+	}
 
-  next();
-});
+	next();
+}); */
 
-// server requests
+// Routes
 
 app.use('/api/notes', noteRouter);
 app.use('/api/folders', folderRouter);
 
 // errorHandler middleware
 
-app.use(function errorHandler(error, req, res, next){
-  let response;
-  if (NODE_ENV === 'production') {
-    response = {error: {message: 'server error'}};
-  } else {
-    console.error(error);
-    response = {message: error.message, error};
-  }
-  res.status(500).json(response);
+app.use(function errorHandler(error, req, res, next) {
+	let response;
+	if (NODE_ENV === 'production') {
+		response = { error: { message: 'server error' } };
+	} else {
+		console.error(error);
+		response = { message: error.message, error };
+	}
+	res.status(500).json(response);
 });
 
 // exports
